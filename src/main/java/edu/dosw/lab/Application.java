baf4266698ce62632364c5eec31bba4774a19311
Elizabeth Correa <elizabeth.correa-s@mail.escuelaing.edu.co>
2025-08-29 16:59:04 -0500

    Implementacion application.java

--- src/main/java/edu/dosw/lab/Application.java
+++ src/main/java/edu/dosw/lab/Application.java
@@ -1,5 +1,8 @@
 package edu.dosw.lab;
 
-public class Application {
 
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Ejecutando Laboratorio 3");
+    }
 }

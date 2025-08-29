63958772772ee9fd7a5da7d4b1143ab058b63714
Juanseom <juanse.om13@gmail.com>
2025-08-29 18:21:39 -0500

    Parte 2 - Reto #3: Sebastian Ortega - Se añadio la estructura de clases que se usaran para el reto 3 usando el principio Solid de Single Responsability

---
+++ src/main/java/edu/dosw/lab/Laboratorio_3_DOSW_Salamanca_Correa_Beltran/Solid/HistoriaUsuario.java
@@ -0,0 +1,4 @@
+package edu.dosw.lab.Laboratorio_3_DOSW_Salamanca_Correa_Beltran.Solid;
+
+public class HistoriaUsuario {
+}
---
+++ src/main/java/edu/dosw/lab/Laboratorio_3_DOSW_Salamanca_Correa_Beltran/Solid/PlanningPoker.java
@@ -0,0 +1,8 @@
+package edu.dosw.lab.Laboratorio_3_DOSW_Salamanca_Correa_Beltran.Solid;
+
+
+public class PlanningPoker {
+    public static void main(String[] args) {
+    }
+
+}
